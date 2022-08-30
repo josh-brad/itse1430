d520c52a3849d32e545a771f1387c738795da6ec
josh-brad <joshua.bradshaw197@my.tccd.edu>
2022-08-29 21:05:43 -0500

    Started Moive Lib

---
+++ classwork/Section1/MovieLibary.ConsoleHost/Program.cs
@@ -0,0 +1,14 @@
+﻿//Movie definition
+string title = "";
+String discription = "";
+int runLength = 0; //in minutes
+int releaseYear = 1900;
+string rating = "";
+bool isClassic = false;
+
+Console.WriteLine("Enter a title: ");
+Console.WriteLine("Enter an optional description: ");
+Console.WriteLine("Enter a run legthe (in minutes): "); 
+Console.WriteLine("enter the release year: ");
+Console.WriteLine("Entering the MPAA: ");
+Console.WriteLine("Is this a classic? ");
--- classwork/Section1/Section1/Program.cs
+++ classwork/Section1/Section1/Program.cs
@@ -11,3 +11,28 @@ value = Console.ReadLine();
 double payRate = Double.Parse(value);
 
 Console.WriteLine("Your pay is" + (hours * payRate));
+
+//Varible Declaration 
+int x;
+double distanceFromMoon = 0;
+distanceFromMoon = 0;
+char letterGrade;
+bool isPassing;
+
+string name;
+string firstname = "Dave", lastname = "Bob";
+
+firstname = lastname = name;
+//int iHours
+//Float fRate
+
+
+
+
+//Block Statment
+{
+    decimal payRate;
+}
+
+//distanceFromMoon = 10.5;
+isPassing = distanceFromMoon > 0;
