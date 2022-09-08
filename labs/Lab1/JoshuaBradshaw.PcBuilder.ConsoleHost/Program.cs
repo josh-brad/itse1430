b0c9aeabb54f61d3590d5d5723ab9c86b3f62fbf
Joshua Bradshaw <joshua.bradshaw197@my.tccd.edu>
2022-09-07 19:06:04 -0500

    Lab 1

--- labs/Lab1/JoshuaBradshaw.PcBuilder.ConsoleHost/Program.cs
+++ labs/Lab1/JoshuaBradshaw.PcBuilder.ConsoleHost/Program.cs
@@ -2,6 +2,8 @@
 Console.WriteLine("ITSE 1430");
 Console.WriteLine("Fall Semester");
 
+Console.WriteLine("Press Anything to Start");
+Console.ReadLine();
 void Menu ()
 {
     Console.WriteLine("Start Order");
