b70e4dccbdfd309297f46e3b00fbd7f56c01e025
josh-brad <joshua.bradshaw197@my.tccd.edu>
2022-09-26 19:37:15 -0500

    change

--- classwork/MovieLibrary/MovieLibary/Movie.cs
+++ classwork/MovieLibrary/MovieLibary/Movie.cs
@@ -1,22 +1,24 @@
 ﻿namespace MovieLibary
-{
-    /// <summary>
-    /// Represents a movie.
-    /// </summary>
+{    
+    /// <summary>Represents a movie.</summary>
     public class Movie
     {
-        
         private string _title = "";
+
         public string GetTitle ()
         {
             return _title;
         }
+
         public void SetTitle ( string title )
         {
+            //this._title = title;
             _title = title;
         }
-        //TODO Hide this
+
+        //TODO: Hide this...
         public string _description = "";
+        
         public int _runLength = 0; //in minutes
         public int _releaseYear = 1900;
         public string _rating = "";
@@ -24,32 +26,86 @@
 
         public bool IsBlackAndWhite ()
         {
-            return _releaseYear <1939;
+            return _releaseYear < 1939;
         }
-       /// <summary>Clones the existing movie. </summary>
-       /// <returns>A copy of the movie.</returns>
+
+        /// <summary>Clones the existing movie.</summary>
+        /// <returns>A copy of the movie.</returns>
         public Movie Clone ()
         {
-            var movie = new Movie ();
-            CopyTo (movie);
+            var movie = new Movie();
+            CopyTo(movie);
 
             return movie;
         }
 
-       /// <summary>Copy the movie to another instance. </summary>
-       /// <param name="movie">Movie to copy into.</param>
-        public void CopyTo ( /* Movie this */Movie movie )
+        /// <summary>Copy the movie to another instance.</summary>
+        /// <param name="movie">Movie to copy into.</param>
+        public void CopyTo ( /* Movie this */ Movie movie )
         {
             //var areEqual = title == this.title;
-            
+
             movie._title = _title;
-            movie._description = _description;
+            movie._description = _description; //this.description
             movie._runLength = _runLength;
             movie._releaseYear = _releaseYear;
             movie._rating = _rating;
             movie._isClassic = _isClassic;
         }
+    }
+}namespace MovieLibrary
+{    
+    /// <summary>Represents a movie.</summary>
+    public class Movie
+    {
+        private string _title = "";
 
-       
+        public string GetTitle ()
+        {
+            return _title;
+        }
+
+        public void SetTitle ( string title )
+        {
+            //this._title = title;
+            _title = title;
+        }
+
+        //TODO: Hide this...
+        public string _description = "";
+        
+        public int _runLength = 0; //in minutes
+        public int _releaseYear = 1900;
+        public string _rating = "";
+        public bool _isClassic = false;
+
+        public bool IsBlackAndWhite ()
+        {
+            return _releaseYear < 1939;
+        }
+
+        /// <summary>Clones the existing movie.</summary>
+        /// <returns>A copy of the movie.</returns>
+        public Movie Clone ()
+        {
+            var movie = new Movie();
+            CopyTo(movie);
+
+            return movie;
+        }
+
+        /// <summary>Copy the movie to another instance.</summary>
+        /// <param name="movie">Movie to copy into.</param>
+        public void CopyTo ( /* Movie this */ Movie movie )
+        {
+            //var areEqual = title == this.title;
+
+            movie._title = _title;
+            movie._description = _description; //this.description
+            movie._runLength = _runLength;
+            movie._releaseYear = _releaseYear;
+            movie._rating = _rating;
+            movie._isClassic = _isClassic;
+        }
     }
 }
