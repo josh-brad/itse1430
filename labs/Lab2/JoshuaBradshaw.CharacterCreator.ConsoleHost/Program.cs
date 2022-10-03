4d05fe11e8deb5175843f1652665e5a9de513c41
Joshua Bradshaw <joshua.bradshaw197@my.tccd.edu>
2022-10-02 22:51:44 -0500

    lab 2 stuff

--- labs/Lab2/JoshuaBradshaw.CharacterCreator.ConsoleHost/Program.cs
+++ labs/Lab2/JoshuaBradshaw.CharacterCreator.ConsoleHost/Program.cs
@@ -1,5 +1,91 @@
 ﻿using JoshuaBradshaw.CharacterCreator;
 
-Character myCharacter = new Character("Bob", "Hunter", "Elf");
+//Character myCharacter = new Character("Bob", "Hunter", "Elf");
+//Console.WriteLine(myCharacter.Name);
+//myCharacter.Strength = 99;
+//Console.WriteLine(myCharacter.Strength);
 
-Console.WriteLine(myCharacter.GetName());
+const ConsoleKey viewCharacterCommand = ConsoleKey.S;
+
+Character? theCharacter = null;
+bool done = false;
+
+do
+{
+    DisplayMenu();
+    ConsoleKeyInfo key = Console.ReadKey();
+    switch (key.Key)
+    {
+        case ConsoleKey.A: theCharacter = AddCharacter();                  
+        break;
+        case viewCharacterCommand : ViewCharacter(theCharacter);        
+        break;
+    }
+
+} while (!done);
+
+Character AddCharacter ()
+{
+    Character character = new Character();
+
+    character.Name = ReadString("Enter Character Name", true);
+    character.Race = ReadString("Enter Race", true);
+    character.Profession = ReadString("Enter Profession", true);
+    character.Biography = ReadString("Enter Optional Biography", false);
+    character.Strength = ReadInt("Enter Strength", 1, 100);
+    character.Intelligence = ReadInt("Enter Intelligence", 1, 100);
+    character.Agility = ReadInt("Enter Agility", 1, 100);
+    character.Constitution = ReadInt("Enter Constitution", 1, 100);
+    character.Charisma = ReadInt("Enter Charisma", 1, 100);
+    return character;    
+}
+
+void ViewCharacter (Character? character)
+{
+
+    if (character == null)
+    {
+        Console.WriteLine("No Character");
+        return;
+    }
+
+    Console.WriteLine(character.ToString());
+}
+
+string? ReadString (string message, bool required )
+{
+    Console.WriteLine(message);
+
+    while (true)
+    {
+        string? value = Console.ReadLine();
+
+        if (value != null || !required)
+            return value;
+        else
+            Console.WriteLine("Value is required");
+    }
+}
+
+int ReadInt (string message, int minValue, int maxValue )
+{
+    Console.WriteLine(message);
+
+    while (true)
+    {
+        string? value = Console.ReadLine();
+
+        if (Int32.TryParse(value, out var result) && result >= minValue && result <= maxValue)
+            return result;
+        else
+            Console.WriteLine($"Value must be between {minValue} and {maxValue}");
+
+    }
+}
+
+void DisplayMenu ()
+{
+    Console.WriteLine("A) Add Character");
+    Console.WriteLine("Q) Quit");
+    Console.WriteLine( $"{viewCharacterCommand}) View");
+}
--- labs/Lab2/JoshuaBradshaw.CharacterCreator/Character.cs
+++ labs/Lab2/JoshuaBradshaw.CharacterCreator/Character.cs
@@ -8,41 +8,78 @@ namespace JoshuaBradshaw.CharacterCreator
 {
     public class Character
     {
-        private string _name;
-        private string _profession;
-        private string _race;
-        private string _biography;
+
         private int _strength;
+        public int Strength 
+        {
+            get { return _strength; }
+            set { _strength = VerifyStats(value) ? value: 55; }
+        }
+
         private int _intelligence;
+        public int Intelligence
+        {
+            get { return _intelligence; }
+            set { _intelligence = VerifyStats(value) ? value : 55; }
+        }
+
         private int _agility;
+        public int Agility 
+        {
+            get { return _agility; }
+            set { _agility = VerifyStats(value) ? value : 55; }
+        }
+
         private int _constitution;
+        public int Constitution 
+        {
+            get { return _constitution; }
+            set { _constitution = VerifyStats(value) ? value : 55; }
+        }
+
         private int _charisma;
+        public int Charisma 
+        {
+            get { return _charisma; }
+            set { _charisma = VerifyStats(value) ? value : 55; }
+        }
+       
+        private string _race;
+        public string Race 
+        {
+            get { return String.IsNullOrEmpty(_race) ? "" : _race; }
+            set { _race = String.IsNullOrEmpty(value) ? "" : value.Trim(); }
+        }
+       
+        private string _biography;
+        public string Biography
+        {
+            get { return String.IsNullOrEmpty(_biography) ? "" : _biography; }
+            set { _biography = String.IsNullOrEmpty(value) ? "" : value.Trim(); }
+        }
+        
+        private string _name;
+        public string Name
+        {
+            get { return String.IsNullOrEmpty(_name) ? "" : _name; }
+            set { _name = String.IsNullOrEmpty(value) ? "" : value.Trim(); }
+        }
+        
+        private string _profession;
+        public string Profession
+        {           
+            get { return String.IsNullOrEmpty(_name) ? "" : _profession; }
+            set { _profession = String.IsNullOrEmpty(value) ? "" : value.Trim(); }           
+        }
 
-        public Character ( string name, string profession, string race )
-        {
-            _name = name;
-            _profession = profession;
-            _race = race;
-        }
-        public string GetName ()
-        {
-            return _name;
-        }
 
-        public string GetProfession()
-        {
-            return _profession;
-        }
-
-        public string GetRace ()
-        {
-            return _race;
-        }
-         
-        public string GetBiography ()
-        {
-            return _biography;
-        }
+        ////public Character ( string name, string profession, string race )
+        ////{
+        ////    _name = name;
+        ////   _profession = profession;
+        ////  _race = race;
+        ////}
+       
 
         public void SetStrength (int strength)
         {
@@ -56,6 +93,12 @@ namespace JoshuaBradshaw.CharacterCreator
         {
             return stat <= 100 && stat >= 1;                           
         }
+
+        public override string ToString()
+        {
+            return $"Name: {Name}\n";
+        }
+
     }
 }
 
